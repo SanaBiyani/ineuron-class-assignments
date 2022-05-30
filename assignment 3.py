76f04c2d62051bffb72bc4f69f195e5955bc1fda
SanaBiyani <104782922+SanaBiyani@users.noreply.github.com>
2022-05-30 15:56:34 +0530

    Add files via upload

---
+++ assignment 3.py
@@ -0,0 +1,45 @@
+
+#Q1.
+# "What will the output be of ((500//7) % 5) ** 3?"
+#assumed output = 1
+print(((500//7) % 5) ** 3)
+#output = 1
+
+
+#Q2.
+# "How will you extract ‘love’ from the string S = “I love Python”? (More than one option may be correct.)."
+x = "I love Python"
+print((x.split()[1]))
+#output = love
+
+#Q3.
+# "What will the output of 3 * 3 ** 3 be?"
+#assumed output = 81
+print(3 * 3 ** 3)
+#output = 81
+
+#Q4.
+# "If you have a tuple T = (3, 5, 7, 11), what will the output of T.append(9) be?"
+#assumed output = error
+#output = error
+
+#Q5.
+# "What will the output of the following program be?"
+# "L1 = ['Vikas', 'Akshay', 'Sanskar', 'Mahima']
+# "print(L1[1][-1])"
+#assumed output = 'y'
+L1 = ['Vikas', 'Akshay', 'Sanskar', 'Mahima']
+print(L1[1][-1])
+#output = 'y'
+
+#Q6.
+# "What will be the output ?\n",
+#"l = [32, 34, 12, 27, 33]\n",
+# "l.append([14, 19])\n",
+#"print(len(l))"
+#assumed output = 6
+l = [32, 34, 12, 27, 33]
+l.append([14, 19])
+print(len(l))
+#output = 6
+
